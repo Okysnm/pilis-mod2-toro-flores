142c72c6f3f71c79ade7198fac380208ef82a3d9
oscar toro <okysnm@gmail.com>
2022-08-26 20:08:14 -0300

    subuendo archivos

---
+++ main.js
@@ -0,0 +1,33 @@
+function onClick (event) {
+    event.preventDefault();
+    
+    const mensaje = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('message').value
+    }
+    console.log(mensaje);
+  
+  
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(mensaje),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => { 
+          console.log(json);
+          Swal.fire(
+              'Enviado',
+              'Gracias por tu comentario', 
+              'success'
+          );
+          cleanForm();
+      })
+      .catch((err) => console.log(err));
+  
+  }
+  function cleanForm() {
+      let formulario = document.getElementById('formulario');    
+      formulario.reset();    
+  }
